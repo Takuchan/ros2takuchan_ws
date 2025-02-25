764d19cbe01961a87a81e17161bc4427e4d0ea9f
Takuchan <takuchanapp@gmail.com>
2025-02-25 14:23:00 +0900

    megarover3の外部パーツは表示できているが、本体のオブジェクトが表示されていない

--- megarover_sim/setup.py
+++ megarover_sim/setup.py
@@ -1,22 +1,30 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'megarover_sim'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/launch/utils', glob('launch/utils/*.py')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.xacro')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.gazebo')),
+    ('share/' + package_name + '/meshes/mega3', glob('meshes/mega3/*.stl')),  
+        ('share/' + package_name + '/worlds/gz', glob('worlds/gz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='i2lab',
-    maintainer_email='i2lab@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Takuchan',
+    maintainer_email='takuchanapp@gmail.com',
+    description='Megarover3をGazeboで動かすROS2シミュレーションパッケージ',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
